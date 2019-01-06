8e68e2db3620aecf92270a0e06fa2049c3b21469
896660689 <896660689@qq.com>
2019-01-06 14:55:15 +0800

    Update adbchk.sh

--- adbchk.sh
+++ adbchk.sh
@@ -23,6 +23,6 @@ if [ -f "/tmp/adb/bin/adbybyupdate.sh" ];then
 	fi
 	rm -f /tmp/adbyby.mem /tmp/lazy.txt /tmp/video.txt /tmp/local-md5.json /tmp/md5.json
 	rm -f /tmp/adb/data/*.bak
-	sleep 5 && /tmp/adb/ad_gz
+	sleep 5 && /tmp/adb/adbchk.sh
 fi
 [ -f "/tmp/adbyby.mem" ] rm -f /tmp/adbyby.mem
