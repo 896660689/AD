884494951443b0008d6ea20166c1225875c8b1b1
896660689 <896660689@qq.com>
2019-01-11 20:20:13 +0800

    Update adbyby.sh

--- file/adbyby.sh
+++ file/adbyby.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-## Adaptation Grassland in Lucheng 2019.01.10
+## Adaptation Grassland in Lucheng 2019.01.11
 
 username=`nvram get http_username`
 Firewall_rules="/etc/storage/post_iptables_script.sh"
@@ -8,24 +8,27 @@ ad_home="/tmp/adb"
 
 if [ -f "$ad_home/bin/adbyby" ]; then
 	port=$(iptables -t nat -L | grep 'ports 8118' | wc -l)
-	logger "adbyby" "找到$port个8118透明代理端口,正在关闭..."
-	while [[ "$port" -ge 1 ]]
-	do
+	if [[ "$port" -ge 1 ]]; then
+		logger "adbyby" "找到$port个 8118 透明代理端口,正在关闭..."
 		iptables -t nat -D PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8118
 		port=$(iptables -t nat -L | grep 'ports 8118' | wc -l)
-	done
-	echo -e "\e[1;31m 关闭 adbyby 进程,代理端口任务 \e[0m"
-	touch /tmp/cron_adb.lock
-	if [ -n "`pidof adbyby`" ] ; then
-		kill -9 "`pidof adbyby`"
 	fi
-	logger "adbyby" "adbyby 进程已成功关闭..."
+	PIDS=$(ps |grep "$ad_home/bin/adbyby" |grep -v "grep" |wc -l)
+	if [ $PIDS -eq 0 ]; then
+		echo -e "\e[1;31m 没发现 adbyby 程序进程... \e[0m"
+	else
+		if [ -n "`pidof adbyby`" ] ; then
+			kill -9 "`pidof adbyby`"
+			logger "adbyby 进程已成功关闭..."
+		fi
+	fi
 	if [ -f "/etc/storage/cron/crontabs/$username" ]; then
 		grep "adb" /etc/storage/cron/crontabs/$username
 		if [ $? -eq 0 ]; then
 			echo "YES"
 		else
 			sed -i '$a 30 5 * * * /bin/sh /tmp/adb/ad_up >/dev/null 2>&1' /etc/storage/cron/crontabs/$username
+			logger "adbyby 添加定时更新任务..."
 		fi
 	fi
 	export PATH=/opt/sbin:/opt/bin:/usr/sbin:/usr/bin:/sbin:/bin
@@ -51,15 +54,16 @@ if [ -f "$ad_home/bin/adbyby" ]; then
 	sleep 3
 	check=$(ps |grep "$ad_home/bin/adbyby" |grep -v "grep" | wc -l)
 	if [ "$check" = 0 ]; then
-		logger "adbyby" "adbyby启动失败..."
+		logger "adbyby" "adbyby 启动失败..."
 		exit 0
 	else
 		port=`iptables -t nat -L |grep 'ports 8118' |wc -l`
 		[ $port -eq 0 ] && iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8118
 		logger "adbyby" "adbyby 进程守护已启动..."
 	fi
+	echo -e "\e[1;31m adbyby 开始更新规则... \e[0m" && logger "adbyby 开始更新规则..."
+	/tmp/adb/ad_up && sleep 3; exit 0
 else
-	echo -e "\e[1;31m 没有发现 adbyby 程序,没能启动... \e[0m"	 
+	echo -e "\e[1;31m 没有发现 adbyby 程序,没能启动... \e[0m"
+	exit 0
 fi
-echo -e "\e[1;31m adbyby 开始更新规则... \e[0m" && logger "adbyby 开始更新规则..."
-/tmp/adb/ad_up && sleep 3; exit 0
